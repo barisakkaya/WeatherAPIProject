8ab7b1c916c346c589c9c8922260d59142b7b471
Barış Can Akkaya <>
2021-06-13 21:31:01 +0300

    Edited VC

--- WeatherAPI/View/ViewController.swift
+++ WeatherAPI/View/ViewController.swift
@@ -9,18 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cityName: UITextField!
+    //var weatherViewModel: WeatherViewModel? = nil
+    //let url: String? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=a09ee526355bc421b09df90fe76ee86c&units=metric"
-        WeatherRequest().getResponse(url: url) { weatherModel in
-            if let model = weatherModel {
-                DispatchQueue.main.async {
-                    print(model.main.temp)
-                }
-            }
-        }
+        cityName.delegate = self
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gesture)
     }
-    
-    
 }
 
+
+//MARK: - ViewController
+
+extension ViewController {
+    @IBAction func findTemp(_ sender: UIButton) {
+        
+    }
+    
+    func getURL(cityName: String?) -> String {
+        if let cityName = cityName {
+            let url = "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=a09ee526355bc421b09df90fe76ee86c&units=metric"
+            return url
+        }
+        return "Error"
+    }
+}
+
+
+
+//MARK: - TextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    @objc func hideKeyboard() {
+        cityName.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("11")
+    }
+    
+}
